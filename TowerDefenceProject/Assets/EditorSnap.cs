b388483b28a34c97bbfeffda982f34542a550d48
Dan <danielpinnington12@gmail.com>
2019-02-02 19:08:25 +0000

    Adding Materials
    Materials imported from Unity Store.

--- TowerDefenceProject/Assets/EditorSnap.cs
+++ TowerDefenceProject/Assets/EditorSnap.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
-        Debug.Log("Editor causes this Awake");
-    }
+    [SerializeField] float gridSize = 10f;
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 snapPosition;
-        snapPosition.x = Mathf.RoundToInt(transform.position.x / 10f) * 10f; //Takes position, divides by 10 and rounds the X position to nearest number. 
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; //Takes position, divides by 10 and rounds the X position to nearest number. 
                                                                              //Example: 6 / 10 = 0.6 (RoundToInt = 1)
                                                                              //Finally: 1 * 10f = 10f.
-        snapPosition.z = Mathf.RoundToInt(transform.position.z / 10f) * 10f;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPosition.x, 0f, snapPosition.z);
     }
