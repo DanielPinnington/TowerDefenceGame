b910fa2ff1995e0b177092b7d5e8c0a311a49b03
Dan <danielpinnington12@gmail.com>
2019-02-09 17:26:26 +0000

    Added Directions for Player (script)
    (Up, Down, Left, Right) Directions added for player within path finder script, may add verticles later.

--- TowerDefenceProject/Assets/PathFinder.cs
+++ TowerDefenceProject/Assets/PathFinder.cs
@@ -7,6 +7,12 @@ public class PathFinder : MonoBehaviour
     [SerializeField] Waypoint startWayPoint, endWayPoint; //Allows start and end waypoints, set in 'world'.
     //  Vector2 is a Key, WavePoint is the value. 'grid' is the name.
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up, // Up
+        Vector2Int.right, //Right
+        Vector2Int.down, //Down
+        Vector2Int.left //Left
+    };
 
     void Start()
     {
